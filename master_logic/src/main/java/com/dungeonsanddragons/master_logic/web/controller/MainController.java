e099e6efe6de8ee95b7cf5da375f020cb9d23825
Camille <camille.simiand@enalean.com>
2020-12-17 13:32:16 +0100

    MS master-logic set up with configuration to use eureka

---
+++ master_logic/src/main/java/com/dungeonsanddragons/master_logic/web/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.dungeonsanddragons.master_logic.web.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+
+
+//    public deroulement() {
+//
+//    }
+
+//    public launchDice() {
+//
+//    }
+//
+//    public avancer() {
+//
+//    }
+}
---
+++ master_logic/src/test/java/com/dungeonsanddragons/master_logic/MasterLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungeonsanddragons.master_logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
