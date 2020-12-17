b0a300fb77e3e4b896f2bf0582bb9d22d21b7d82
Camille <camille.simiand@enalean.com>
2020-12-17 17:21:04 +0100

    Clean master-logic controllers and correct zone info personnage

--- master_logic/src/main/java/com/dungeonsanddragons/master_logic/web/controller/PlayerController.java
+++ master_logic/src/main/java/com/dungeonsanddragons/master_logic/web/controller/PlayerController.java
@@ -1,23 +1,16 @@
 package com.dungeonsanddragons.master_logic.web.controller;
 
-import com.dungeonsanddragons.master_logic.MasterLogicApplication;
 import com.dungeonsanddragons.master_logic.web.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.slf4j.Logger;
 
 @RestController
 public class PlayerController {
 
     @Autowired
     private RestTemplate restTemplate;
-
-    private Logger logger= MasterLogicApplication.logger(this);
 
     @Value("${resourceUrlListPerso}")
     private String resourceUrlListPerso;
@@ -27,8 +20,6 @@ public class PlayerController {
     }
 
     public Player updatePlayer(Player player) {
-        Player player_updated= restTemplate.patchForObject(resourceUrlListPerso + "update", player, Player.class);
-        logger.info(String.valueOf(player_updated));
-        return player_updated;
+        return restTemplate.patchForObject(resourceUrlListPerso + "update", player, Player.class);
     }
 }
